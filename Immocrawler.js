44da9feab0617f082ee569e1f5d2993ee8adc49c
William Knoepp <waknoepp@hotmail.com>
2021-07-13 14:23:55 +0200

    Implemented basic bot, no storage yet, everything is in memory

---
+++ Immocrawler.js
@@ -0,0 +1,68 @@
+var Crawler = require("node-webcrawler");
+var url = require('url');
+
+let store = {};
+
+function ImmoCrawler(_url_string) {
+    console.log("NEW CRAWLER WHO THIS");
+    this.url_string = _url_string;
+    let new_entity = {};
+    new_entity.results = {};
+    new_entity.all_results = {};
+    new_entity.lastFetch = undefined; 
+    store[this.url_string] = new_entity; 
+    this.crawler = new Crawler({
+        maxConnections : 10,
+        callback : filter_results
+    });
+}
+
+function filter_results(err, res, $) {
+    if(err){
+        console.log(err);
+        return; 
+    }
+
+    let results = []; 
+    $(".result-list-entry__brand-title-container").each((i, e) => {
+        results[i] = e.attribs.href; 
+    });
+    results = results.filter((e) => {
+        return e.includes('expose');
+    });
+    results.forEach((e, i) => {
+        results[i] = e.substring(8);
+    });
+    storeResults(res.uri, results); 
+}
+
+function storeResults(id, results) {
+    let entity = store[id];
+    if (!entity) return;
+    for (i in results) {
+        let result = results[i]; 
+        if (entity.all_results[result]) continue;
+        entity.all_results[result] = {};
+        entity.results[result] = {}; 
+    }
+}
+
+ImmoCrawler.prototype.fetch = function fetch() {
+    // Fetch ImmoScout URL if last fetch was more than 10s ago 
+    let lastFetch = store[this.url_string].lastFetch;
+    let now = Date.now(); 
+    if (lastFetch === undefined || now - lastFetch > 10000) {
+        this.crawler.queue(this.url_string);
+        store[this.url_string].lastFetch = now; 
+    }
+}
+
+ImmoCrawler.prototype.getResults = function getResults() {
+    let id = this.url_string
+    if (!(store[id] && store[id].results && store[id].all_results)) return {};
+    let res = store[id].results;
+    store[id].results = {}; 
+    return res; 
+}
+
+module.exports = ImmoCrawler; 
---
+++ app.js
@@ -0,0 +1,52 @@
+const { Telegraf } = require('telegraf');
+const ImmoCrawler = require('./Immocrawler');
+process.env.BOT_TOKEN = "1773464054:AAHQlAofGmk5BEmTeNr2aK4Ig2LTy70qlyg"
+const bot = new Telegraf(process.env.BOT_TOKEN)
+
+/**
+ * Using: 
+ * Telegram Framework: https://www.section.io/engineering-education/telegram-bot-in-nodejs/
+ * Webcrawler: https://www.npmjs.com/package/node-webcrawler
+ * Serverside jQuery: https://cheerio.js.org/interfaces/cheerioapi.html
+ */
+var state = undefined;
+var clients = {}
+console.log( ImmoCrawler );
+bot.command('start', ctx => {
+    clients[ctx.chat.id] = {};
+    console.log(ctx.from);
+    bot.telegram.sendMessage(ctx.chat.id, 'Welcome! \nGive me an ImmoScout search link and I will send you new results as soon as they get available 🏠 🔍');
+});
+
+bot.on('text', (ctx) => {
+    let message = ctx.update.message.text;
+    client = {}; 
+    client.url = message;
+    client.crawler = new ImmoCrawler(message); 
+    clients[ctx.chat.id] = client; 
+    ctx.reply(`Looking for new results on: ${ctx.update.message.text}`); 
+});
+
+bot.launch();
+console.log("Launched Telegram bot");
+
+function fetchResults() {
+    //console.log("Fetching: ", clients);
+    for (chatID in clients) {
+        client = clients[chatID]; 
+        //console.log("This is client: ", client);
+        if (!(client && client.crawler && client.url)) continue;
+        //console.log("Client: ", client.url);
+        client.crawler.fetch();
+        let resultIDs = client.crawler.getResults();
+        if (resultIDs != {}) sendResults(chatID, resultIDs); 
+    }
+}
+
+function sendResults(chatID, resultIDs) {
+    for (resultID in resultIDs) {
+        bot.telegram.sendMessage(chatID, "https://www.immobilienscout24.de/expose/" + resultID); 
+    }
+}
+
+setInterval(fetchResults, 120000); // Start fetch every two minutes
